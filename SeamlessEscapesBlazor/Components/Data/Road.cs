e26ec5b5fd33984c2bff6f79d80ecc2fde8fb780
Nathan <nluiten1@ewu.edu>
2024-05-24 11:49:48 -0700

    Trying to get Firebase working
    Added firebaseInit.js file and changed some of the register page to try to add new user to database. But when I hit register lead to non existent page.

--- SeamlessEscapesBlazor/Components/Data/Road.cs
+++ SeamlessEscapesBlazor/Components/Data/Road.cs
@@ -3,7 +3,7 @@
 
 namespace SeamlessEscapesBlazor.Models
 {
-    public class Road
+    public class Road : Transport
     {
         [Required, StringLength(100)]
         public string RoadTransportType { get; set; } // e.g., Bus, Car Rental
