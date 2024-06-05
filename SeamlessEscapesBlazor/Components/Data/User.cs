d4083321491489058b95158a6abd62424ef9ab33
Nathan <nluiten1@ewu.edu>
2024-06-04 17:42:32 -0700

    DB

--- SeamlessEscapesBlazor/Components/Data/User.cs
+++ SeamlessEscapesBlazor/Components/Data/User.cs
@@ -28,9 +28,9 @@ namespace SeamlessEscapesBlazor.Models
 {
     public class User
     {
-        public int UserID { get; set; }
         public string Username { get; set; }
         public string Email { get; set; } // Ensure this is not nullable
         public string Password { get; set; }
+        public int UserID { get; set; }
     }
 }
