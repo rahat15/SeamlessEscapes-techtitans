3253321a91b54f70bd7d57ef395062d6ef9a1f84
Nathan <nluiten1@ewu.edu>
2024-06-06 11:03:29 -0700

    Database work
    Nothing accomplished

--- SeamlessEscapesBlazor/Components/Data/ApplicationDbContext.cs
+++ SeamlessEscapesBlazor/Components/Data/ApplicationDbContext.cs
@@ -56,3 +56,4 @@ namespace SeamlessEscapesBlazor.Data
 }
 
 
+
---
+++ SeamlessEscapesBlazor/wwwroot/site.js
@@ -0,0 +1,6 @@
+﻿window.setFormName = function (formName) {
+    const formElement = document.querySelector('form');
+    if (formElement) {
+        formElement.setAttribute('name', formName);
+    }
+};
